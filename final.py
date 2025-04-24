814204f46611cdd0f3a36a61467ad4eda5a53600
Gabrielllloo <gabrielvicenteorallo@gmail.com>
2025-04-24 09:20:08 -0400

    wcnm

--- final.py
+++ final.py
@@ -2,8 +2,15 @@ class books():
     def __init__(self):
         self.name="Henry"
         self.title=""
-   
+        self.books={}
+        self.price=0
     def Add(self):
      self.title=input("what book would you add")
-     print("You successly add it",self.name)
+     print("You successly add", self.title)
+    
+     self._Name=input("enter the name of the book you would like to add:\n").upper
+     self._Author=input("enter the Author of the book youhave just added:\n").upper
+     self._Price=float(input("Enter the listing price of the book:\n"))
+     self._Book[self.title]={'Author':self._Author, 'price':self._Price}
+     print(self._Book)
         
